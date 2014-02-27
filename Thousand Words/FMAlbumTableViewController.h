7bea86c442b9dbc77cff9a245ac3bd2ee1d8b1d1
Fred Myers <fred@fred-myers.com>
2014-02-27 16:20:55 -0500

    Added story board and Create and setup FMAlbumTableViewController.

---
+++ Thousand Words/FMAlbumTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  FMAlbumTableViewController.h
+//  Thousand Words
+//
+//  Created by Fredrick Myers on 2/27/14.
+//  Copyright (c) 2014 Fredrick Myers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FMAlbumTableViewController : UITableViewController
+
+@property (strong, nonatomic) NSMutableArray *albums;
+
+@end
